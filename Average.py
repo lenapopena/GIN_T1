beb3f752f086651be6afca27485794d95168c254
lenapopena <99950937+lenapopena@users.noreply.github.com>
2024-10-25 17:04:48 +0200

    .py exercises 16.10

---
+++ Average.py
@@ -0,0 +1,19 @@
+print("Input 5 Grades: ")
+
+print("Grade 1: ")
+num1 = int(input())
+
+print("Grade 2: ")
+num2 = int(input())
+
+print("Grade 3: ")
+num3 = int(input())
+
+print("Grade 4: ")
+num4 = int(input())
+
+print("Grade 5: ")
+num5 = int(input())
+
+avg = (num1 + num2 + num3 + num4 + num5) / 5
+print(avg)
